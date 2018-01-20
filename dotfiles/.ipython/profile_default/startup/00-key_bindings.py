e1a7c95098dc5f82e559fc81ba0a4ac5296078fb
Jakub Łuczyński <jakub.luczynski@codilime.com>
2018-01-20 19:35:51 +0100

    disabled ipython shortcuts causing problems in older versions

--- dotfiles/.ipython/profile_default/startup/00-key_bindings.py
+++ dotfiles/.ipython/profile_default/startup/00-key_bindings.py
@@ -15,8 +15,8 @@ if getattr(ip, 'pt_cli'):
     handle(Keys.ControlE, filter=insert_mode)(get_by_name('end-of-line'))
     handle(Keys.ControlB, filter=insert_mode)(get_by_name('backward-char'))
     handle(Keys.ControlF, filter=insert_mode)(get_by_name('forward-char'))
-    handle(Keys.Escape, 'b', filter=insert_mode)(get_by_name('backward-word'))
-    handle(Keys.Escape, 'f', filter=insert_mode)(get_by_name('forward-word'))
+    #  handle(Keys.Escape, 'b', filter=insert_mode)(get_by_name('backward-word'))
+    #  handle(Keys.Escape, 'f', filter=insert_mode)(get_by_name('forward-word'))
     handle(Keys.ControlK, filter=insert_mode)(get_by_name('kill-line'))
     handle(Keys.ControlY, filter=insert_mode)(get_by_name('yank'))
     handle(
