20dc3464744ff01e3fae50bc9ba5d794c56d4c03
Jakub Łuczyński <doubleloop@o2.pl>
2020-10-28 14:21:41 +0100

    bash git-prompt
    * fixed white chars

--- dotfiles/.git-prompt-colors.sh
+++ dotfiles/.git-prompt-colors.sh
@@ -1,5 +1,5 @@
 override_git_prompt_colors() {
-  GIT_PROMPT_THEME_NAME="Custom"
+    GIT_PROMPT_THEME_NAME="Custom"
     GIT_PROMPT_START_USER="${Green}\u@\h ${Yellow}\w${ResetColor}"
     GIT_PROMPT_END_USER=" \n${White}${ResetColor}$ "
     GIT_PROMPT_END_ROOT=" \n${White}${ResetColor}# "
