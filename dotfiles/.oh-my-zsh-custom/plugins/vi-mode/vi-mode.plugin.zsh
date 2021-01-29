351822aec369d8b076ece3f5f22aa6ad2ee281b4
Jakub Łuczyński <doubleloop@o2.pl>
2021-01-29 17:56:37 +0100

    zsh vi-mode plugin
    * updates based on oh-my-zsh version

--- dotfiles/.oh-my-zsh-custom/plugins/vi-mode/vi-mode.plugin.zsh
+++ dotfiles/.oh-my-zsh-custom/plugins/vi-mode/vi-mode.plugin.zsh
@@ -21,17 +21,21 @@ fix-prompt() {
 }
 
 zle-keymap-select() {
+    # zle reset-prompt
+    # zle -R
     setcursorshape
-    zle reset-prompt
 }
 zle -N zle-keymap-select
 
-function reset-prompt-accept-line() {
-    _normalmode_cursor
-    zle accept-line
+function zle-line-init() {
+    setcursorshape
 }
-zle -N reset-prompt-accept-line
-bindkey '^M' reset-prompt-accept-line
+zle -N zle-line-init
+
+function zle-line-finish() {
+    _normalmode_cursor
+}
+zle -N zle-line-finish
 
 # allow ctrl-p, ctrl-n for navigate history (standard behaviour)
 bindkey '^P' up-history
@@ -70,5 +74,42 @@ bindkey '^[t' transpose-words
 
 bindkey -M vicmd 'gg' beginning-of-buffer-or-history
 bindkey -M vicmd 'G' end-of-buffer-or-history
+
+autoload -Uz edit-command-line
+zle -N edit-command-line
 bindkey '^x^e' edit-command-line
 bindkey -M vicmd '^x^e' edit-command-line
+bindkey -M vicmd 'vv' edit-command-line
+
+function wrap_clipboard_widgets() {
+  # NB: Assume we are the first wrapper and that we only wrap native widgets
+  # See zsh-autosuggestions.zsh for a more generic and more robust wrapper
+  local verb="$1"
+  shift
+
+  local widget
+  local wrapped_name
+  for widget in "$@"; do
+    wrapped_name="_zsh-vi-${verb}-${widget}"
+    if [ "${verb}" = copy ]; then
+      eval "
+        function ${wrapped_name}() {
+          zle .${widget}
+          printf %s \"\${CUTBUFFER}\" | clipcopy 2>/dev/null || true
+        }
+      "
+    else
+      eval "
+        function ${wrapped_name}() {
+          CUTBUFFER=\"\$(clippaste 2>/dev/null || echo \$CUTBUFFER)\"
+          zle .${widget}
+        }
+      "
+    fi
+    zle -N "${widget}" "${wrapped_name}"
+  done
+}
+
+wrap_clipboard_widgets copy vi-yank vi-yank-eol  vi-delete kill-buffer
+wrap_clipboard_widgets paste vi-put-{before,after} yank
+unfunction wrap_clipboard_widgets
